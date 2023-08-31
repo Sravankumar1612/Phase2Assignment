eaebf3ce72680a2032e0c147c5eff38fa09917e4
Sravankumar1612 <139097275+Sravankumar1612@users.noreply.github.com>
2023-08-31 15:00:18 +0530

    Add files via upload

---
+++ Controllers/TeamPageDbController.cs
@@ -0,0 +1,24 @@
+﻿using Assignment13.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment13.Controllers
+{
+    public class TeamPageDbController : Controller
+    {
+        List<TeamPage> teamPages = new List<TeamPage>() { 
+            new TeamPage(){TeamId = 1,TeamName="India",NOCW="2"},
+            new TeamPage(){TeamId = 2,TeamName="Australia",NOCW="4"},
+            new TeamPage(){TeamId = 3,TeamName="WestIndies",NOCW="2"},
+            new TeamPage(){TeamId = 4,TeamName="England",NOCW="1"},
+        };
+        // GET: TeamPageDb
+        public ActionResult Index()
+        {
+            return View(teamPages);
+        }
+    }
+}
---
+++ Models/TeamPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment13.Models
+{
+    public class TeamPage
+    {
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public string NOCW { get; set; }
+    }
+}
